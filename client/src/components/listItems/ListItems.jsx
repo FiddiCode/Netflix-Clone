711fae3c9d6ae7332aaab87eb891badee469e34c
fiddicode <coderfardeen@gmail.com>
2023-07-11 13:28:25 +0530

    modified:   client/src/App.scss 	modified:   client/src/components/listItems/ListItems.jsx 	modified:   client/src/components/listItems/listItems.scss

--- client/src/components/listItems/ListItems.jsx
+++ client/src/components/listItems/ListItems.jsx
@@ -3,7 +3,8 @@ import './listItems.scss'
 
 const ListItems = () => {
   return (
-    <div className='listItems'>list
+    <div className='listItems'>
+      <img  src='https://th.bing.com/th/id/OIP.bwlWhMUt-oDRsjAdZ0C1VQHaEK?pid=ImgDet&rs=1'  alt='missimg'/>
     </div>
   )
 }
