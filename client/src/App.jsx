f2e0a627a9ebbb229ec00d8b0502cb10f89df5d1
fiddicode <coderfardeen@gmail.com>
2023-07-12 10:30:12 +0530

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import './App.scss'
 import Watch from './pages/Watch/Watch';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
-import {BrowserRouter as Router,Route,Link, Routes} from 'react-router-dom';
+import {BrowserRouter as Router,Route, Routes, Navigate} from 'react-router-dom';
 function App() {
+  const user=true;
   return (
       <Router>
     <Routes>
@@ -23,14 +24,20 @@ function App() {
 
         {//Above Code is decepreated after react-router-dom v6
          }
+   
+          
+        <Route path="/" element={user ? <Home /> : <Navigate to="/register" />}/>
+        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+        <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/movies" element={<Home type='movies'/>} />
-        <Route path="/series" element={<Home type='series' />} />
-        <Route path="/watch" element={<Watch />} />
-
+        { user &&
+          (<>  
+          <Route path="/movies" element={<Home type='movies'/>} />
+          <Route path="/series" element={<Home type='series' />} />
+          <Route path="/watch" element={<Watch />} />
+         </>)
+         }
+    
 
   </Routes>
   </Router>
