8d2fc7a4a8da834772fb6e75fdb0ae80948aecfd
fiddicode <coderfardeen@gmail.com>
2023-07-12 10:53:38 +0530

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/components/navbar/navbar.jsx 	modified:   client/src/pages/Home/home.jsx

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -2,6 +2,7 @@ import Featured from '../../components/featured/Featured.jsx';
 import Navbar from '../../components/navbar/navbar.jsx';
 import List from '../../components/list/List.jsx';
 import './home.scss'
+import axios from 'axios'
 const Home=({type})=>{
 
 return(
