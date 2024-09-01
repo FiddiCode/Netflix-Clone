ebf88781a320c686212accebddb2e588ea51521d
Fardeen Qureshi <coderfardeen@gmail.com>
2024-09-01 23:38:11 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Home from './pages/Home/home'
-import './App.scss'
+ import './App.scss'
 import Watch from './pages/Watch/Watch';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
